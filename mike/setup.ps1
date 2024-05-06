a636292dc70a6a7244303d63959aeaa11542451c
Mike Griese <migrie@microsoft.com>
2024-05-06 11:25:25 -0500

    checkpoint

--- mike/setup.ps1
+++ mike/setup.ps1
@@ -1,7 +1,10 @@
-
+winget install --id Microsoft.Powershell
 # Call update-path.ps1 in this folder
 . $PSScriptRoot\update-path.ps1
 
+git config --global --add safe.directory E:/build/terminalbuild2024demo/
+git config --global --add safe.directory E:/build/terminalbuild2024demo/mike/terminal-1.21.1161.0/settings/terminal
+git config --global --add safe.directory E:/build/sudo
 
 # Delete .\mike\terminal-1.21.1161.0\settings\terminal if it exists, and create a new one
 Remove-Item -Recurse -Force $PSScriptRoot\terminal-1.21.1161.0\settings\terminal
