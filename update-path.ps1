13692feb0ffeae71f808f3560623f16cc6a6cd23
Mike Griese <migrie@microsoft.com>
2024-05-09 14:01:14 -0500

    update terminal version

--- update-path.ps1
+++ update-path.ps1
@@ -4,11 +4,11 @@ $directoryToAdd = $(Get-Location)
 $currentPath = [System.Environment]::GetEnvironmentVariable("PATH", "User")
 
 # Check if the directory is already in the PATH
-if ($currentPath -notlike "*$directoryToAdd*") {
+if ($currentPath -notlike "*$directoryToAdd") {
     # If not, add it to the PATH
     $newPath = $currentPath + ";" + $directoryToAdd
     [System.Environment]::SetEnvironmentVariable("PATH", $newPath, "User")
     Write-Host "Directory added to PATH: $directoryToAdd"
 } else {
-    Write-Host "Directory is already in PATH."
+    Write-Host "Directory '$directoryToAdd' is already in PATH."
 }
